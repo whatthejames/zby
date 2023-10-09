dcf2d765890f08e348a07c241029077ab6a05fcc
whatthejames <124553067+whatthejames@users.noreply.github.com>
2023-10-09 15:25:42 +0600

    Update functions.php

--- funct/functions.php
+++ funct/functions.php
@@ -1,5 +1,5 @@
 <?php  
-$botToken = "sk_live_NITvPzPCNXrK05Gawm1VAB3A"; // Enter ur bot token
+$botToken = "6453459357:AAGdG44mfzFHmz7Ijwi3wJcZUDTMgruZUJ4"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 function GetStr($string, $start, $end){
 $str = explode($start, $string);
